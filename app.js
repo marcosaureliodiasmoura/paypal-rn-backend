844475d86abe21a46f501d421a83b6b7eee24367
MarcosMoura <marcos_moura@live.com>
2019-06-27 10:02:26 -0300

    Versão Inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const engines = require("consolidate");
+const paypal = require("paypal-rest-sdk");
+
+const app = express();
+
+app.engine("ejs", engines.ejs);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Works!");
+});
+
+app.listen(3005, () => {
+  console.log("Server is running");
+});
